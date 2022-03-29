055a5eb2dea7a7b34168364083347316453d505e
th3w0rst <chomanti.limited@gmail.com>
2022-03-29 22:12:15 +0300

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const express = require("express");
 const mainRoute = require("./mainRoute.js")
-
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 console.log(process.env.DATABASE_URL)
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use("/family", mainRoute)
 
