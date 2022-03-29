d4b861b4f3e9e798db0713cd43f13943e54e65a4
th3w0rst <chomanti.limited@gmail.com>
2022-03-29 23:53:25 +0300

    fixed endpoint

--- mainRoute.js
+++ mainRoute.js
@@ -1,8 +1,8 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("./mainController.js")
-router.get("", controller.getFamily);
 router.get("/member", controller.getMember);
+router.get("", controller.getFamily);
 router.post("", controller.createMember);
 router.delete("", controller.deleteMember);
 router.put("", controller.updateMember);
